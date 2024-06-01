821df932ce76a4fbded7911a7ac265692f4275ca
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2024-06-01 19:54:40 +0530

    Add files via upload

---
+++ Odd Even Problem.cpp
@@ -0,0 +1,46 @@
+class Solution {
+  public:
+    string oddEven(string s) {
+        unordered_map<char,int>mp;
+        for(auto &ch:s)
+            mp[ch]++;
+        int x=0,y=0;
+        for(auto it:mp){
+            if((it.first-'a')%2==0 && it.second%2==1)
+                y++;
+            if((it.first-'a')%2==1 && it.second%2==0)
+                x++;
+        }
+        if((x+y)%2==0)
+            return "EVEN";
+        return "ODD";
+    }
+};
+
+/*
+    Given a string s of lowercase English characters, determine whether the summation of x and y is EVEN or ODD.
+    where:
+      1.x is the count of distinct characters that occupy even positions in the English alphabet and have even frequency. 
+      2.y is the count of distinct characters that occupy odd positions in the English alphabet and have odd frequency.
+    Ex: string = "ab" here 'a' has an odd(1) position in the English alphabet & has an odd(1) frequency in the string so a is odd but b has an even(2) position in the English alphabet & has odd(1) frequency so it doesn't count(since string doesn't have 2 b's) so the final answer x + y = 1+0 = 1(odd) would be "ODD".
+    Note: Return "EVEN" if the sum of x & y is even otherwise return "ODD".
+    
+    Example 1:
+    Input: 
+    s = "abbbcc"
+    Output: 
+    ODD
+    Explanation: 
+    x = 0 and y = 1 so (x + y) is ODD. 'a' occupies 1st place(odd) in english alphabets and its frequency is odd(1), 'b' occupies 2nd place(even) but its frequency is odd(3) so it doesn't get counted and 'c' occupies 3rd place(odd) but its frequency is even(2) so it also doesn't get counted.
+    
+    Example 2:
+    Input: 
+    s = "nobitaa"
+    Output: 
+    EVEN
+    Explanation: 
+    Here n, b, t & a would not count since doesn't match with the even condition but o & i will be counted as it satisfies the odd conditions so x = 0 and y = 2 so (x + y) is EVEN.
+
+    T.C : O(n)
+    S.C : O(26)
+*/
